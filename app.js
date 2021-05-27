1892c61cb6857e35ed60f8155e6e1c81149dbe6d
gabriel <gabrielalcantarabarros524@gmail.com>
2021-05-26 22:06:53 -0300

    adicionado alguns comentários

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const vm = new Vue({
       const track = document.querySelector(".bgSlide");
       const telaMobile = window.matchMedia("(max-width: 450px)");
 
+      // o seguinte código aumenta ou diminui o span bgSlide conforme o tamanho
+      // da tela, criando a 'parte' verde do input range à esquerda da thumb
       if (telaMobile.matches) {
         track.style.width = `calc(${this.slide}*2.47px)`;
       } else track.style.width = `calc(${this.slide}*3.8px)`;
